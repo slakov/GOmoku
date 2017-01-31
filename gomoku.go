e2f76ae69037ab86f9a923ef33df16cb133ae0e4
slakov <slakov@gmail.com>
2017-01-31 14:16:26 +0100

    print board

--- gomoku.go
+++ gomoku.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
+	boardSize := 8
 	fmt.Println("gomoku here")
+
+	// print empty board
+	var emptyLine string
+	emptyLine = strings.Repeat(".", boardSize)
+	for i := 0; i < boardSize; i++ {
+		fmt.Println(emptyLine)
+	}
 }
