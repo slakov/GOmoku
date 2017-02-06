d0e6aa61f874cc967ce4884134322e7d59bbc13d
slakov <slakov@gmail.com>
2017-02-06 13:36:20 +0100

    getting move

--- gomoku.go
+++ gomoku.go
@@ -5,14 +5,33 @@ import (
 	"strings"
 )
 
-func main() {
-	boardSize := 8
-	fmt.Println("gomoku here")
+var boardSize int = 8
+var boardArea int = boardSize * boardSize
+var board string
 
-	// print empty board
-	var emptyLine string
-	emptyLine = strings.Repeat(".", boardSize)
+func initBoard() {
+	board = strings.Repeat(".", boardArea)
+}
+
+func printBoard(b string) {
+
 	for i := 0; i < boardSize; i++ {
-		fmt.Println(emptyLine)
+		firstIndex := i * boardSize
+		lastIndex := i*boardSize + boardSize
+		fmt.Println(b[firstIndex:lastIndex])
 	}
+
+}
+
+func main() {
+
+	fmt.Println("gomoku here")
+	initBoard()
+	printBoard(board)
+
+	var x int
+	//var y int
+
+	fmt.Scan(&x)
+
 }
