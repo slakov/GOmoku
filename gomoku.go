8dea4d212e8c5416b052e21c2e9a86bf3d60556d
slakov <slakov@gmail.com>
2017-02-08 12:09:07 +0100

    regular commit

--- gomoku.go
+++ gomoku.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 	"strings"
 )
 
@@ -15,9 +16,27 @@ func initBoard() {
 
 func printBoard(b string) {
 
+	columnLabels := make([]int, boardSize)
+
+	for i := range columnLabels {
+		columnLabels[i] = 1 + i
+	}
+
+	valuesText := []string{}
+	valuesText = append(valuesText, " ")
+
+	for i := range columnLabels {
+		number := columnLabels[i]
+		text := strconv.Itoa(number)
+		valuesText = append(valuesText, text)
+	}
+
+	fmt.Println(strings.Join(valuesText, ""))
+
 	for i := 0; i < boardSize; i++ {
 		firstIndex := i * boardSize
 		lastIndex := i*boardSize + boardSize
+		fmt.Print(i + 1)
 		fmt.Println(b[firstIndex:lastIndex])
 	}
 
